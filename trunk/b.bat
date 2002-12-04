12db1ebb7675260c30083f09b8ec884ad4bc0cd3
Julian Todd <julian@goatchurch.org.uk>
2002-12-04 09:36:41 +0000

    [svn] This was out of date stuff

--- trunk/b.bat
+++
@@ -1,1 +0,0 @@
-c:\jdk1.4\bin\javac -classpath c:\jdk1.4\jaxp-1.1\jaxp.jar -d . src\%1.java
--- trunk/src/HelloTunn.java
+++
@@ -1,8 +0,0 @@
-package Tunnel; 
-class HelloTunn
-{
-	public static void main(String args[]) 
-	{
-		System.out.println("Hello from package Tunnel"); 
-	}
-}
