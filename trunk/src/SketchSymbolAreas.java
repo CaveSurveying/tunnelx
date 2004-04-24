c9c6227e681969337f07b2121a711c25f9371b70
goatchurch <goatchurch@localhost>
2004-04-24 11:32:38 +0100

    [svn] debug and more efficiency in not repeating areas

--- trunk/src/SketchSymbolAreas.java
+++ trunk/src/SketchSymbolAreas.java
@@ -41,100 +41,28 @@ class ConnectiveComponentAreas
 {
 	Vector vconnpaths = new Vector();
 	Vector vconnareas = new Vector();
-	Area saarea;
-    int[] overlapcomp = new int[50];
+	Area saarea = null;
+    int[] overlapcomp = new int[55];
     int noverlapcomp = 0;
 
 
-	/////////////////////////////////////////////
-	boolean Checkopinvconnpath(OnePath op)
-    {
-    	for (int i = 0; i < vconnpaths.size(); i++)
-			if (((RefPathO)vconnpaths.elementAt(i)).op == op)
-				return true;
-		return false;
-    }
 
-	/////////////////////////////////////////////
-	// make list of areas, and the joined area.
-	// get connective paths to connect to this object
-	/////////////////////////////////////////////
-	static RefPathO rpot = new RefPathO();
-	static RefPathO rpop = new RefPathO();
-	ConnectiveComponentAreas(OnePath op, int liconncompareaindex)
+	boolean CompareConnAreaList(Vector lvconn)
 	{
-		assert op.linestyle == SketchLineStyle.SLS_CONNECTIVE;
-		assert op.iconncompareaindex == -1;
-		// leave op.iconncomareaindex unset so we track back along it.
-		int ivcc = -1;
+		if (lvconn.size() != vconnareas.size())
+			return false;
+		for (int i = 0; i < lvconn.size(); i++)
+			if (vconnareas.elementAt(i) != lvconn.elementAt(i))
+				return false;
+		return true;
+	}
 
-		// spread through this connected component completely
-		// spreading with the sector at each node we meet.
-		while (ivcc < vconnpaths.size())
-		{
-			// -1 is special case for having just started, and avoiding edge going into array twice
-			RefPathO rpo = (ivcc != -1 ? (RefPathO)vconnpaths.elementAt(ivcc) : new RefPathO(op, true));
-			rpot.ccopy(rpo);
-            rpop.ccopy(rpo);
-
-			// scan round to find the preceding edge
-			do
-			{
-				if ((rpot.op.linestyle != SketchLineStyle.SLS_CONNECTIVE) && (rpot.op.linestyle != SketchLineStyle.SLS_CENTRELINE))
-		            rpop.ccopy(rpot);
-			} while (!rpot.AdvanceRoundToNode(rpo));
-
-			// scan from this edge to next, or to this edge
-			rpot.ccopy(rpop);
-			while (true)
-			{
-				boolean brepback = rpot.AdvanceRoundToNode(rpop);
-				if (rpot.op.linestyle == SketchLineStyle.SLS_CONNECTIVE)
-				{
-                	if (rpot.op.iconncompareaindex == -1)
-					{
-						assert !Checkopinvconnpath(rpot.op);
-	                	rpot.op.iconncompareaindex = liconncompareaindex;
-						vconnpaths.addElement(new RefPathO(rpot.op, !rpot.bFore));
-					}
-
-					// if we can connect to it, it should be in this list already
-					else
-	                {
-	                	assert rpot.op.iconncompareaindex == liconncompareaindex;
-						assert Checkopinvconnpath(rpot.op);
-					}
-				}
-				else if (rpot.op.linestyle == SketchLineStyle.SLS_CENTRELINE)
-					; // this type doesn't bound the sector.
-				else
-					break;
-				if (brepback)
-					break;
-			}
-			ivcc++;
-		}
-
-		// now we have all the components, we make the set of areas for this component.
-		OneSArea.iamarkl++;
-		for (int i = 0; i < vconnpaths.size(); i++)
-		{
-			OnePath sop = ((RefPathO)vconnpaths.elementAt(i)).op;
-			sop.iconncompareaindex = liconncompareaindex;
-			if ((sop.kaleft != null) && (sop.kaleft.iamark != OneSArea.iamarkl))
-			{
-				vconnareas.addElement(sop.kaleft);
-				sop.kaleft.iamark = OneSArea.iamarkl;
-   			}
-			if ((sop.karight != null) && (sop.karight.iamark != OneSArea.iamarkl))
-			{
-				vconnareas.addElement(sop.karight);
-				sop.karight.iamark = OneSArea.iamarkl;
-   			}
-		}
+	ConnectiveComponentAreas(Vector lvconnpaths, Vector lvconnareas)
+	{
+		vconnpaths.addAll(lvconnpaths);
+		vconnareas.addAll(lvconnareas);
 
 		// now make the combined area here
-		saarea = null;
 		for (int i = 0; i < vconnareas.size(); i++)
 		{
 			Area aarea = ((OneSArea)vconnareas.elementAt(i)).aarea;
@@ -157,6 +85,112 @@ class SketchSymbolAreas
 {
 	Vector vconncom = new Vector(); // ConnectiveComponents
 
+	void InsertConnArea(Vector lvconnareas, OneSArea ka)
+	{
+		// insert in order please
+		ka.iamark = OneSArea.iamarkl;
+
+		int i = 0;
+		for ( ; i < lvconnareas.size(); i++)
+			if (ka.hashCode() < lvconnareas.elementAt(i).hashCode())
+				break;
+		lvconnareas.insertElementAt(ka, i);
+	}
+
+	/////////////////////////////////////////////
+	void SetConnComparIndex(Vector lvconnpaths, int liconncompareaindex)
+	{
+		for (int i = 0; i < lvconnpaths.size(); i++)
+		{
+			RefPathO rpo = (RefPathO)lvconnpaths.elementAt(i);
+			assert (rpo.op.iconncompareaindex != -1) && (rpo.op.iconncompareaindex >= liconncompareaindex);
+			rpo.op.iconncompareaindex = liconncompareaindex;
+		}
+	}
+
+
+	/////////////////////////////////////////////
+	boolean Checkopinvconnpath(Vector lvconnpaths, OnePath op)
+    {
+    	for (int i = 0; i < lvconnpaths.size(); i++)
+			if (((RefPathO)lvconnpaths.elementAt(i)).op == op)
+				return true;
+		return false;
+    }
+
+	/////////////////////////////////////////////
+	// make list of areas, and the joined area.
+	// get connective paths to connect to this object
+	/////////////////////////////////////////////
+	static RefPathO rpot = new RefPathO();
+	static RefPathO rpop = new RefPathO();
+	void SetConnComp(Vector lvconnpaths, Vector lvconnareas, OnePath op, int liconncompareaindex)
+	{
+		assert op.linestyle == SketchLineStyle.SLS_CONNECTIVE;
+		assert op.iconncompareaindex == -1;
+		// leave op.iconncomareaindex unset so we track back along it.
+		int ivcc = -1;
+
+		// spread through this connected component completely
+		// spreading with the sector at each node we meet.
+		while (ivcc < lvconnpaths.size())
+		{
+			// -1 is special case for having just started, and avoiding edge going into array twice
+			RefPathO rpo = (ivcc != -1 ? (RefPathO)lvconnpaths.elementAt(ivcc) : new RefPathO(op, true));
+			rpot.ccopy(rpo);
+            rpop.ccopy(rpo);
+
+			// scan round to find the preceding edge
+			do
+			{
+				if ((rpot.op.linestyle != SketchLineStyle.SLS_CONNECTIVE) && (rpot.op.linestyle != SketchLineStyle.SLS_CENTRELINE))
+		            rpop.ccopy(rpot);
+			} while (!rpot.AdvanceRoundToNode(rpo));
+
+			// scan from this edge to next, or to this edge
+			rpot.ccopy(rpop);
+			while (true)
+			{
+				boolean brepback = rpot.AdvanceRoundToNode(rpop);
+				if (rpot.op.linestyle == SketchLineStyle.SLS_CONNECTIVE)
+				{
+                	if (rpot.op.iconncompareaindex == -1)
+					{
+						assert !Checkopinvconnpath(lvconnpaths, rpot.op);
+	                	rpot.op.iconncompareaindex = liconncompareaindex;
+						lvconnpaths.addElement(new RefPathO(rpot.op, !rpot.bFore));
+					}
+
+					// if we can connect to it, it should be in this list already
+					else
+	                {
+	                	assert rpot.op.iconncompareaindex == liconncompareaindex;
+						assert Checkopinvconnpath(lvconnpaths, rpot.op);
+					}
+				}
+				else if (rpot.op.linestyle == SketchLineStyle.SLS_CENTRELINE)
+					; // this type doesn't bound the sector.
+				else
+					break;
+				if (brepback)
+					break;
+			}
+			ivcc++;
+		}
+
+		// now we have all the components, we make the set of areas for this component.
+		OneSArea.iamarkl++;
+		for (int i = 0; i < lvconnpaths.size(); i++)
+		{
+			OnePath sop = ((RefPathO)lvconnpaths.elementAt(i)).op;
+			sop.iconncompareaindex = liconncompareaindex;
+			if ((sop.kaleft != null) && (sop.kaleft.iamark != OneSArea.iamarkl))
+				InsertConnArea(lvconnareas, sop.kaleft);
+			if ((sop.karight != null) && (sop.karight.iamark != OneSArea.iamarkl))
+				InsertConnArea(lvconnareas, sop.karight);
+		}
+	}
+
 	// make all the connected symbol areas
 	void MakeSSA(Vector vpaths)
 	{
@@ -164,12 +198,39 @@ class SketchSymbolAreas
 		for (int i = 0; i < vpaths.size(); i++)
 			((OnePath)vpaths.elementAt(i)).iconncompareaindex = -1;
 
+		Vector lvconnpaths = new Vector();
+		Vector lvconnareas = new Vector();
 		for (int i = 0; i < vpaths.size(); i++)
 		{
 			OnePath op = (OnePath)vpaths.elementAt(i);
 			if ((op.linestyle == SketchLineStyle.SLS_CONNECTIVE) && (op.iconncompareaindex == -1))
-				vconncom.addElement(new ConnectiveComponentAreas(op, vconncom.size()));
+			{
+				SetConnComp(lvconnpaths, lvconnareas, op, i);
+
+				// find if this is new or not
+				ConnectiveComponentAreas mcca = null;
+				int i1;
+				for (i1 = 0; i1 < vconncom.size(); i1++)
+				{
+					mcca = (ConnectiveComponentAreas)(vconncom.elementAt(i1));
+					if (mcca.CompareConnAreaList(lvconnareas))
+						break;
+					mcca = null;
+				}
+
+				// we have a match by area
+				SetConnComparIndex(lvconnpaths, i1); // i1 is a match of vconncom.size()
+				if (mcca != null)
+					mcca.vconnpaths.addAll(lvconnpaths);
+				// no match
+				else
+					vconncom.addElement(new ConnectiveComponentAreas(lvconnpaths, lvconnareas));
+
+				lvconnpaths.clear();
+				lvconnareas.clear();
+			}
 		}
+
 
 		// find overlapping components
 		// do with a sort in future
@@ -191,8 +252,7 @@ class SketchSymbolAreas
 				}
 			}
 		}
-
-		TN.emitMessage("Unique group areas: " + vconncom.size());
+		System.out.println("Conn connective areas n = " + vconncom.size()); 
 	}
 
 	// this is used only for the drawing of a selected hatched overlay to see what areas the symbol will be restricted to.
