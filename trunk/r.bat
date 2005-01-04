dc333e2bda05f4def9f72c0a62308d3e86dc050a
goatchurch <goatchurch@localhost>
2005-01-04 23:02:57 +0000

    [svn] batch files

--- trunk/r.bat
+++ trunk/r.bat
@@ -1,1 +1,2 @@
-"%JAVA_HOME%bin\java" -showversion -esa -Xmx200M -classpath batik-1.5.1\lib\batik-svggen.jar;batik-1.5.1\lib\batik-dom.jar;batik-1.5.1\lib\batik-awt-util.jar;batik-1.5.1\lib\batik-util.jar;batik-1.5.1\lib\batik-xml.jar;. Tunnel.MainBox --verbose 
+java -showversion -esa -Xmx200M -classpath  Tunnel.MainBox --verbose
+
