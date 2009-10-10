6482958d526e4bdb03dbf371f9e13c757a76da4c
Julian Todd <julian@goatchurch.org.uk>
2009-10-10 19:58:23 +0100

    first use of java3d technology

---
+++ trunk/src/PassageFloor3D.java
@@ -0,0 +1,493 @@
+////////////////////////////////////////////////////////////////////////////////
+// TunnelX -- Cave Drawing Program
+// Copyright (C) 2002  Julian Todd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+////////////////////////////////////////////////////////////////////////////////
+package Tunnel;
+
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+import java.awt.Color;
+import java.awt.BasicStroke;
+import java.awt.Stroke;
+import java.awt.Font;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.PathIterator;
+import java.io.IOException;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.FlatteningPathIterator;
+
+
+
+//package org.jdesktop.j3d.examples.gears;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.geom.Point2D;
+
+import java.util.ArrayList; 
+import java.util.List; 
+
+import javax.swing.JPanel; 
+import javax.swing.JFrame; 
+
+import com.sun.j3d.utils.universe.SimpleUniverse;
+import com.sun.j3d.utils.universe.ViewingPlatform;
+
+import com.sun.j3d.utils.geometry.GeometryInfo;
+import com.sun.j3d.utils.geometry.NormalGenerator;
+//import com.sun.j3d.utils.geometry.Triangulator;
+
+import javax.media.j3d.Canvas3D;
+import javax.media.j3d.BoundingSphere;
+import javax.media.j3d.DirectionalLight;
+import javax.media.j3d.AmbientLight;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Background;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.TriangleFanArray;
+import javax.media.j3d.GeometryArray;
+import javax.media.j3d.TransformGroup;
+import javax.media.j3d.Material;
+import javax.media.j3d.Appearance;
+import javax.media.j3d.Transform3D;
+import javax.media.j3d.PolygonAttributes; 
+
+import javax.vecmath.Vector3f;
+import javax.vecmath.Point3d;
+import javax.vecmath.Point3f;
+import javax.vecmath.Color3f;
+
+import com.sun.j3d.utils.behaviors.vp.OrbitBehavior; // mouse controls
+
+////////////////////////////////////////////////////////////////
+public class pyvtkGraphics2D extends JFrame
+{
+    private SimpleUniverse univ = null;
+    private BranchGroup scene = null;
+    private JPanel drawingPanel;
+    
+    private double[] m_VertexArray = { 1, 1, 0, //0
+      0, 3, 0, //1
+      1, 5, 1, //2
+      2, 4, 0, //3
+      4, 5, 0, //4
+      3, 3, 1, //5
+      4, 2, 0, //6
+      4, 0, 0, //7
+      3, 0, 1, //8
+      2, 1, 0, //9
+      // these are vertices for the hole
+      1, 3, 0, //10
+      2, 3, 0, //11
+      3, 2, 1, //12
+      3, 1, 0, //13
+      2, 2, 1 };//14
+
+
+    /////////////////////////////////////////////////////////
+    public Shape3D DcreateShape() 
+    {
+        // Create an Appearance.
+        Appearance look = new Appearance();
+        Color3f objColor = new Color3f(0.95f, 0.5f, 0.6f);
+        Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
+        Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
+        look.setMaterial(new Material(objColor, black, objColor, white, 100.0f));
+        look.setPolygonAttributes(new PolygonAttributes(PolygonAttributes.POLYGON_FILL, PolygonAttributes.CULL_NONE, 0.0f, true)); 
+
+    GeometryInfo gi = new GeometryInfo(GeometryInfo.POLYGON_ARRAY);
+    gi.setCoordinates(m_VertexArray);
+    int[] stripCountArray = { 10, 5 };
+    int[] countourCountArray = { stripCountArray.length };
+    gi.setContourCounts(countourCountArray);
+    gi.setStripCounts(stripCountArray);
+
+//    Triangulator triangulator = new Triangulator();
+//    triangulator.triangulate(gi);
+    NormalGenerator normalGenerator = new NormalGenerator(0.02);
+    normalGenerator.generateNormals(gi);
+
+    return new Shape3D(gi.getGeometryArray(), look);
+    }
+
+    /////////////////////////////////////////////////////////
+    public Shape3D createAreaShape(OneSArea osa) 
+    {
+        // Create an Appearance.
+        Appearance look = new Appearance();
+        Color3f objColor = new Color3f(0.95f, 0.5f, 0.6f);
+        Color3f black = new Color3f(0.0f, 0.0f, 0.0f);
+        Color3f white = new Color3f(1.0f, 1.0f, 1.0f);
+        look.setMaterial(new Material(objColor, black, objColor, white, 100.0f));
+        look.setPolygonAttributes(new PolygonAttributes(PolygonAttributes.POLYGON_FILL, PolygonAttributes.CULL_NONE, 0.0f, true)); 
+
+        List<Point3d> pts = new ArrayList<Point3d>(); 
+        List<Point2D> lpts = new ArrayList<Point2D>();   // for reflecting the edges as we go along them
+
+		// we should perform the hard task of reflecting certain paths in situ.
+		for (RefPathO rpo : osa.refpathsub)
+		{
+            double pathlength = 0.0; 
+            lpts.add(rpo.op.pnstart.pn); 
+            FlatteningPathIterator fpi = new FlatteningPathIterator(rpo.op.gp.getPathIterator(null), fpflatness);
+            while (!fpi.isDone())
+            {
+                int curvtype = fpi.currentSegment(coords);
+                assert (curvtype == (lpts.size() == 1 ? PathIterator.SEG_MOVETO : PathIterator.SEG_LINETO));
+                double xd = coords[0] - lpts.get(lpts.size() - 1).getX(); 
+                double yd = coords[1] - lpts.get(lpts.size() - 1).getY(); 
+                pathlength += Math.sqrt(xd*xd + yd*yd); 
+                lpts.add(new Point2D.Float(coords[0], coords[1])); 
+                fpi.next();
+            }
+            lpts.add(rpo.op.pnend.pn); 
+
+            double z0 = rpo.FromNode().zalt; 
+            double zfac = (rpo.ToNode().zalt - z0) / (pathlength != 0.0 ? pathlength : 1.0); 
+            int i = (rpo.bFore ? 1 : lpts.size() - 2); 
+            double rpathlength = 0.0; 
+            while (rpo.bFore ? (i < lpts.size()) : (i >= 0))
+            {
+                int ip = i - (rpo.bFore ? 1 : -1); 
+                double xd = lpts.get(i).getX() - lpts.get(ip).getX(); 
+                double yd = lpts.get(i).getY() - lpts.get(ip).getY(); 
+                rpathlength += Math.sqrt(xd*xd + yd*yd); 
+                pts.add(new Point3d(lpts.get(i).getX(), lpts.get(i).getY(), z0 + rpathlength*zfac)); 
+                i += (rpo.bFore ? 1 : -1); 
+            }
+
+            lpts.clear(); 
+        }
+        if (pts.size() < 3)
+            return null; 
+
+    GeometryInfo gi = new GeometryInfo(GeometryInfo.POLYGON_ARRAY);
+    gi.setCoordinates(pts.toArray(new Point3d[pts.size()]));
+    //int[] stripCountArray = { 10, 5 };
+    //int[] countourCountArray = { stripCountArray.length };
+    int[] stripCountArray = { pts.size() };
+    int[] countourCountArray = { 1 };
+    gi.setContourCounts(countourCountArray);
+    gi.setStripCounts(stripCountArray);
+
+//    Triangulator triangulator = new Triangulator();
+//    triangulator.triangulate(gi);
+    NormalGenerator normalGenerator = new NormalGenerator(0.02);
+    normalGenerator.generateNormals(gi);
+
+    return new Shape3D(gi.getGeometryArray(), look);
+    }
+
+    /////////////////////////////////////////////////////////
+    public BranchGroup createSketchSceneGraph(OneSketch tsketch) 
+    {
+        // Create the root of the branch graph
+        BranchGroup branchRoot = createBranchEnvironment();
+
+        // Create a Transformgroup to scale all objects so they
+        // appear in the scene.
+        TransformGroup objScale = new TransformGroup();
+        Transform3D t3d = new Transform3D();
+        t3d.setScale(0.04);
+        objScale.setTransform(t3d);
+        branchRoot.addChild(objScale);
+
+        // Create the transform group node and initialize it to the
+        // identity.  Enable the TRANSFORM_WRITE capability so that
+        // our behavior code can modify it at runtime.  Add it to the
+        // root of the subgraph.
+        TransformGroup gearboxTrans = new TransformGroup();
+//        gearboxTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
+//        gearboxTrans.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
+        objScale.addChild(gearboxTrans);
+        
+        gearboxTrans.addChild(DcreateShape());
+        for (OneSArea osa : tsketch.vsareas)
+        {
+            Shape3D shape = createAreaShape(osa); 
+            if (shape != null)
+                gearboxTrans.addChild(shape); 
+        }
+
+        // Have Java 3D perform optimizations on this scene graph.
+        branchRoot.compile();
+        
+        return branchRoot;
+    }
+
+
+
+    /////////////////////////////////////////////////////////
+    BranchGroup createBranchEnvironment()
+    {
+        // Create the root of the branch graph
+        BranchGroup branchRoot = new BranchGroup();
+         
+        // Create a bounds for the background and lights
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000.0);
+
+        // Set up the background
+        Color3f bgColor = new Color3f(0.05f, 0.5f, 0.15f);
+        Background bgNode = new Background(bgColor);
+        bgNode.setApplicationBounds(bounds);
+        branchRoot.addChild(bgNode);
+
+        // Set up the ambient light
+        Color3f ambientColor = new Color3f(0.1f, 0.1f, 0.1f);
+        AmbientLight ambientLightNode = new AmbientLight(ambientColor);
+        ambientLightNode.setInfluencingBounds(bounds);
+        branchRoot.addChild(ambientLightNode);
+
+        // Set up the directional lights
+        Color3f light1Color = new Color3f(1.0f, 1.0f, 0.9f);
+        Vector3f light1Direction  = new Vector3f(1.0f, 1.0f, 1.0f);
+        Color3f light2Color = new Color3f(1.0f, 1.0f, 0.9f);
+	    Vector3f light2Direction  = new Vector3f(-1.0f, -1.0f, -1.0f);
+
+        DirectionalLight light1 = new DirectionalLight(light1Color, light1Direction);
+        light1.setInfluencingBounds(bounds);
+        branchRoot.addChild(light1);
+
+        DirectionalLight light2 = new DirectionalLight(light2Color, light2Direction);
+        light2.setInfluencingBounds(bounds);
+        branchRoot.addChild(light2);
+
+        return branchRoot;
+    }
+
+    /////////////////////////////////////////////////////////
+    private Canvas3D createUniverse() 
+    {
+        // Get the preferred graphics configuration for the default screen
+        GraphicsConfiguration config = SimpleUniverse.getPreferredConfiguration();
+
+        // Create a Canvas3D using the preferred configuration
+        Canvas3D c = new Canvas3D(config);
+        
+        // Create simple universe with view branch
+        univ = new SimpleUniverse(c);
+            
+        // add mouse behaviors to the ViewingPlatform
+        ViewingPlatform viewingPlatform = univ.getViewingPlatform();
+
+        // add orbit behavior to the ViewingPlatform
+        OrbitBehavior orbit = new OrbitBehavior(c, OrbitBehavior.REVERSE_ALL);
+        BoundingSphere bounds = new BoundingSphere(new Point3d(0.0, 0.0, 0.0), 1000.0);
+        orbit.setSchedulingBounds(bounds);
+        viewingPlatform.setViewPlatformBehavior(orbit);
+
+        // This will move the ViewPlatform back a bit so the
+        // objects in the scene can be viewed.
+        univ.getViewingPlatform().setNominalViewingTransform();
+    
+        // Ensure at least 5 msec per frame (i.e., < 200Hz)
+        univ.getViewer().getView().setMinimumFrameCycleTime(5);
+    
+        return c;
+    }
+    
+    
+    /////////////////////////////////////////////////////////
+    public pyvtkGraphics2D() 
+    {
+        // Initialize the GUI components
+        initComponents();
+
+        // Create Canvas3D and SimpleUniverse; add canvas to drawing panel
+        Canvas3D c = createUniverse();
+        drawingPanel.add(c, java.awt.BorderLayout.CENTER);
+        
+    }
+
+
+    /////////////////////////////////////////////////////////
+    private void initComponents() 
+    {
+        drawingPanel = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setTitle("GearBox");
+        drawingPanel.setLayout(new java.awt.BorderLayout());
+
+        drawingPanel.setPreferredSize(new java.awt.Dimension(700, 700));
+        getContentPane().add(drawingPanel, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }
+    
+
+// old crap
+	Shape clip = null;
+	String crgb = "#000000";
+	float calpha = 1.0F; //fill-opacity=".5"
+	float fpflatness = 0.5F;
+	LineOutputStream los;
+
+	////////////////////////////////////////
+	pyvtkGraphics2D(LineOutputStream llos)
+	{
+		los = llos;
+	}
+
+	////////////////////////////////////////
+	// open and close
+	void writeheader(float x, float y, float width, float height) throws IOException
+	{
+		TNXML.chconvleng = TNXML.chconvlengWSP; // a complete hack to stop &space; getting in here
+		los.WriteLine("<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>");
+		los.WriteLine("");
+
+		//String viewbox = String.valueOf(x) + " " + String.valueOf(y) + " " + String.valueOf(width) + " " + String.valueOf(height);
+		//los.WriteLine(TNXML.xcomopen(0, "svg", "width", "12cm", "height", "10cm", "viewBox", viewbox, "xmlns", "http://www.w3.org/2000/svg", "version", "1.1"));
+		//los.WriteLine(TNXML.xcomtext(1, "title", "Example"));
+		//los.WriteLine(TNXML.xcomtext(1, "desc", "description thing"));
+
+		//los.WriteLine(TNXML.xcom(1, "rect", "x", String.valueOf(x), "y", String.valueOf(y), "width", String.valueOf(width), "height", String.valueOf(height), "fill", "none", "stroke", "blue"));
+		los.WriteLine(TNXML.xcomopen(0, "tunnelxpyvtk"));
+	}
+
+	////////////////////////////////////////
+	void writefooter() throws IOException
+	{
+		los.WriteLine(TNXML.xcomclose(0, "tunnelxpyvtk"));
+		TNXML.chconvleng = TNXML.chconv.length;
+	}
+    public void setColor(Color c)
+	{
+		int rgb = c.getRGB();
+		crgb = "#" + Integer.toHexString(rgb & 0xffffff);
+		calpha = ((rgb >> 24) & 255) / 255.0F;
+	}
+    public void setStroke(Stroke s)
+	{
+	}
+    public void setFont(Font f)
+		{ /*System.out.println(f.toString());*/ }
+    public void drawString(String s, float x, float y)
+	{
+		try
+		{
+		los.WriteLine(TNXML.xcom(0, "label", "x", String.valueOf(x), "y", String.valueOf(y), "string", s));
+		}
+		catch (IOException e) {;};
+	}
+	public void draw(Shape s)
+	{
+		//writeshape(s, "none", crgb);
+    }
+	public void fill(Shape s)
+	{
+		//writeshape(s, crgb, "none");
+    }
+
+    public void setClip(Shape lclip)
+	{
+		clip = lclip;
+	}
+    public Shape getClip()
+	{
+		return clip;
+	}
+
+
+	////////////////////////////////////////
+	public void writearea(OneSArea osa) throws IOException
+	{
+		if (osa.subsetattr.areacolour == null)
+			return;
+		if (SketchLineStyle.bDepthColours)
+			setColor(SketchLineStyle.GetColourFromCollam(osa.icollam, true)); 
+		else
+			setColor(osa.subsetattr.areacolour);
+		los.WriteLine(TNXML.xcomopen(0, "pyvtkarea", "colour", crgb));
+
+		// we should perform the hard task of reflecting certain paths in situ.
+		for (RefPathO rpo : osa.refpathsub)
+			writepath(rpo.op.gp, rpo.op.pnstart.zalt, rpo.op.pnend.zalt, rpo.bFore);
+		los.WriteLine(TNXML.xcomclose(0, "pyvtkarea"));
+	}
+
+	////////////////////////////////////////
+	float[] coords = new float[6];
+	public void writepath(GeneralPath gp, float zstart, float zend, boolean bFore) throws IOException
+	{
+		los.WriteLine(TNXML.xcomopen(1, "path", "zstart", String.valueOf(zstart), "zend", String.valueOf(zend), "reversed", (bFore ? "0" : "1")));
+		FlatteningPathIterator fpi = new FlatteningPathIterator(gp.getPathIterator(null), fpflatness);
+		int ptcount = 0;
+		while (!fpi.isDone())
+		{
+			int curvtype = fpi.currentSegment(coords);
+			assert (curvtype == (ptcount == 0 ? PathIterator.SEG_MOVETO : PathIterator.SEG_LINETO));
+			los.WriteLine(TNXML.xcom(2, "pt", "x", String.valueOf(coords[0]), "y", String.valueOf(coords[1])));
+			fpi.next();
+			ptcount++;
+		}
+		los.WriteLine(TNXML.xcomclose(1, "path"));
+	}
+	
+    /////////////////////////////////////////////////////////
+	static void PrintThisPYVTK(OneSketch tsketch)
+    {
+        pyvtkGraphics2D gb = new pyvtkGraphics2D();
+
+        // Create the content branch and add it to the universe
+        gb.scene = gb.createSketchSceneGraph(tsketch);
+        gb.univ.addBranchGraph(gb.scene);
+
+        gb.setVisible(true);
+    }
+
+    /////////////////////////////////////////////////////////
+    public static void main(final String args[]) 
+    {
+        pyvtkGraphics2D gb = new pyvtkGraphics2D();
+
+        // Create the content branch and add it to the universe
+//        gb.scene = gb.createSceneGraph();
+        gb.univ.addBranchGraph(gb.scene);
+
+        gb.setVisible(true);
+    }
+
+	/////////////////////////////////////////////
+	static void DPrintThisPYVTK(OneSketch tsketch)
+	{
+		try
+		{
+		FileAbstraction fout = FileAbstraction.MakeWritableFileAbstraction("pyvtk.xml");
+		TN.emitMessage("Writing file " + fout.getName());
+		LineOutputStream los = new LineOutputStream(fout);
+		pyvtkGraphics2D pyvtk = new pyvtkGraphics2D(los);
+		boolean bRefillOverlaps = false;
+
+		Rectangle2D bounds = tsketch.getBounds(false, false);
+		pyvtk.writeheader((float)bounds.getX(), (float)bounds.getY(), (float)bounds.getWidth(), (float)bounds.getHeight());
+		for (OneSArea osa : tsketch.vsareas)
+		{
+			if (osa.iareapressig == SketchLineStyle.ASE_KEEPAREA)
+				pyvtk.writearea(osa);
+		}
+
+		//tsketch.paintWquality(svgg, bHideCentreline, bHideMarkers, bHideStationNames);
+		pyvtk.writefooter();
+		los.close();
+		}
+		catch (IOException ie)
+		{ System.out.println(ie.toString()); }
+	}
+}
+
+
