a0ba44fc2a1e13a7706e091294d63b93fd9710dc
Julian Todd <julian@goatchurch.org.uk>
2010-08-10 11:11:57 +0100

    fix elevation projection rendering

--- trunk/src/TodeNode.java
+++ trunk/src/TodeNode.java
@@ -35,6 +35,8 @@ import java.awt.event.ActionListener;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.awt.GridLayout;
+
 
 
 /////////////////////////////////////////////
@@ -70,7 +72,6 @@ class DoubleArray
     }
 }
 
-
 /////////////////////////////////////////////
 class TodeNode
 {
@@ -78,8 +79,10 @@ class TodeNode
     DoubleArray spiketimes = new DoubleArray(); 
     List<TodeFibre> outgoingfibres = new ArrayList<TodeFibre>(); 
     List<TodeFibre> incomingfibres = new ArrayList<TodeFibre>(); 
-    DoubleArray currentenvelope = new DoubleArray(); 
+    //DoubleArray currentenvelope = new DoubleArray(); 
     double nextspike = -1; 
+
+    double nodeintensity = 0.0; 
 
     TodeNode(OnePathNode lopn)
     {
@@ -98,6 +101,7 @@ class TodeFibre
 
     Double[] opseglengths; 
 
+    /////////////////////////////////////////////
     TodeFibre(OnePath lop, TodeNode lfromnode, TodeNode ltonode)
     {
         op = lop; 
@@ -105,10 +109,50 @@ class TodeFibre
         tonode = ltonode; 
         timelength = op.linelength / TN.CENTRELINE_MAGNIFICATION; // linear distance; opseglengths[-1] would be spline length
 
+        fromnode.outgoingfibres.add(this); 
+        tonode.incomingfibres.add(this); 
+
         // visualization stuff
         opseglengths = new Double[op.nlines]; 
         for (int i = 0; i < op.nlines; i++)
             opseglengths[i] = (i == 0 ? 0.0 : opseglengths[i - 1]) + op.MeasureSegmentLength(i);  
+
+        envelope.add(0.0); 
+        envelope.add(0.0); 
+        envelope.add(0.1); 
+        envelope.add(1.1); 
+        envelope.add(2.0); 
+        envelope.add(0.0); 
+    }
+
+    /////////////////////////////////////////////
+    double GetIntensity(double eT)
+    {
+        if (envelope.size() == 0)
+            return 0.0; 
+        if (eT < envelope.get(0))
+            return 0.0; 
+        for (int i = 2; i < envelope.size(); i += 2)
+        {
+            if (eT > envelope.get(i)) 
+                continue; 
+            double d = envelope.get(i) - envelope.get(i - 2); 
+            double lam = (d != 0.0 ? (eT - envelope.get(i - 2)) / d : 1.0); 
+            return envelope.get(i - 1) * (1.0 - lam) + envelope.get(i + 1) * lam; 
+        }
+        return 0.0; 
+    }
+
+    /////////////////////////////////////////////
+    void SetPosD(Point2D spos, double dst)
+    {
+        int j = 0; 
+        for ( ; j < opseglengths.length - 1; j++)
+            if (opseglengths[j] >= dst)
+                break; 
+        double ljm1 = (j == 0 ? 0.0 : opseglengths[j-1]); 
+        double tr = (dst - ljm1) / (opseglengths[j] - ljm1); 
+        op.EvalSeg(spos, null, j, tr); 
     }
 }
 
@@ -153,6 +197,65 @@ class TodeNodeCalc
                 todefibres.add(new TodeFibre(op, FindTodeNode(op.pnstart), tonode)); 
         }
     }
+
+
+    /////////////////////////////////////////////
+    class PosSpikeViz
+    {
+        Point2D spos = new Point2D.Double(); 
+        boolean batend; 
+        double intensity; // when batend
+    }
+
+    /////////////////////////////////////////////
+    // this function calculates spikes and intensity independently of the nextspike calculation, and should agree with it
+    List<PosSpikeViz> spikelist = new ArrayList<PosSpikeViz>(); 
+    int nspikelist = 0; 
+    void PosSpikes(double sca) 
+    {
+        nspikelist = 0; 
+        for (TodeNode todenode : todenodes)
+        {
+            todenode.nodeintensity = 0.0; 
+            for (TodeFibre todefibre : todenode.incomingfibres)
+            {
+                for (int i = 0; i < todefibre.fromnode.spiketimes.size(); i++)
+                {
+                    double st = T - todefibre.fromnode.spiketimes.get(i); 
+                    if (st < 0.0)
+                        continue; // only can happen for precoded trains
+                    // spike on the fibre
+                    if (st < todefibre.timelength)
+                    {
+                        if (nspikelist == spikelist.size())
+                            spikelist.add(new PosSpikeViz()); 
+                        double dst = st / todefibre.timelength * todefibre.opseglengths[todefibre.opseglengths.length - 1]; 
+                        spikelist.get(nspikelist).batend = false; 
+                        todefibre.SetPosD(spikelist.get(nspikelist).spos, dst); 
+                        nspikelist++; 
+                        continue; 
+                    }
+
+                    // spike glued to the end of the fibre
+                    double est = st - todefibre.timelength; 
+                    if (est <= todefibre.envelope.get(todefibre.envelope.size() - 2))
+                    {
+                        if (nspikelist == spikelist.size())
+                            spikelist.add(new PosSpikeViz()); 
+                        double fintensity = todefibre.GetIntensity(est); 
+                        double dste = todefibre.opseglengths[todefibre.opseglengths.length - 1] - SketchLineStyle.strokew/2; 
+System.out.println(" GG " + dste + " " + todefibre.opseglengths[todefibre.opseglengths.length - 1] + " " + fintensity + " " + sca); 
+                        double dst = Math.max(todefibre.opseglengths[todefibre.opseglengths.length - 1] - SketchLineStyle.strokew/2, todefibre.opseglengths[todefibre.opseglengths.length - 1] / 2); 
+                        spikelist.get(nspikelist).batend = true; 
+                        todefibre.SetPosD(spikelist.get(nspikelist).spos, dst); 
+                        spikelist.get(nspikelist).intensity = fintensity; 
+                        todenode.nodeintensity += fintensity; 
+                        nspikelist++; 
+                    }
+                }
+            }
+        }
+    }
 }
 
 
@@ -161,6 +264,8 @@ class TodeNodePanel extends JPanel
 {
 	SketchDisplay sketchdisplay;
     JButton buttgeneratetodes = new JButton("Gen Todes"); 
+    JButton buttadvance = new JButton("Advance"); 
+    JTextField tfadvancetime = new JTextField("0.5", 5); 
     JTextField tftime = new JTextField(5); 
 
     TodeNodeCalc tnc; 
@@ -177,7 +282,12 @@ class TodeNodePanel extends JPanel
 		buttgeneratetodes.addActionListener(new ActionListener() 
 			{ public void actionPerformed(ActionEvent e) { GenerateTodes(); } } ); 	
 
+		setLayout(new GridLayout(0, 2));
+
         add(buttgeneratetodes); 
+        add(new JLabel()); 
+        add(buttadvance); 
+        add(tfadvancetime); 
         add(new JLabel("T:")); 
         add(tftime); 
 
@@ -190,13 +300,32 @@ class TodeNodePanel extends JPanel
             catch (NumberFormatException ne)
             {;}
             sketchdisplay.sketchgraphicspanel.repaint(); 
-        }}); 
+        }});
+
+        buttadvance.addActionListener(new ActionListener() 
+            { public void actionPerformed(ActionEvent e)  { AdvanceEvent(); } } ); 
+        tfadvancetime.addActionListener(new ActionListener() 
+            { public void actionPerformed(ActionEvent e)  { AdvanceEvent(); } } ); 
     }
 
 	/////////////////////////////////////////////
     void GenerateTodes()
     {
         tnc = new TodeNodeCalc(sketchdisplay.sketchgraphicspanel.tsketch); 
+        UpdateT(0.0); 
+    }
+
+	/////////////////////////////////////////////
+    void AdvanceEvent()
+    {
+        double advancetime = 1.0; 
+        try
+        {
+            advancetime = Float.parseFloat(tfadvancetime.getText()); 
+        }
+        catch (NumberFormatException ne)
+        {;}
+        UpdateT(tnc.T + advancetime); 
     }
 
 	/////////////////////////////////////////////
@@ -208,35 +337,22 @@ class TodeNodePanel extends JPanel
     }
 
 	/////////////////////////////////////////////
-    Point2D spos = new Point2D.Double(); 
     void painttodenode(GraphicsAbstraction ga)
     {
         if (tnc == null)
             return; 
-        for (TodeNode todenode : tnc.todenodes)
-            ga.drawShape(todenode.opn.Getpnell(), null, Color.green);
+        tnc.PosSpikes(sketchdisplay.sketchgraphicspanel.currtrans.getScaleX()); 
 
-        for (TodeFibre todefibre : tnc.todefibres)
+
+        for (TodeNode todenode : tnc.todenodes)
+            ga.drawShape(todenode.opn.Getpnell(), null, (todenode.nodeintensity == 0.0 ? Color.black : Color.green)); 
+
+        float currstrokew = SketchLineStyle.strokew*0.8F; 
+        for (int i = 0; i < tnc.nspikelist; i++)
         {
-            for (int i = 0; i < todefibre.fromnode.spiketimes.size(); i++)
-            {
-                double st = tnc.T - todefibre.fromnode.spiketimes.get(i); 
-                if ((st >= 0.0) && (st <= todefibre.timelength))
-                {
-                    double dst = st / todefibre.timelength * todefibre.opseglengths[todefibre.opseglengths.length - 1]; 
-                    int j = 0; 
-                    for ( ; j < todefibre.opseglengths.length - 1; j++)
-                        if (todefibre.opseglengths[j] >= dst)
-                            break; 
-                    double ljm1 = (j == 0 ? 0.0 : todefibre.opseglengths[j-1]); 
-                    double tr = (dst - ljm1) / (todefibre.opseglengths[j] - ljm1); 
-                    todefibre.op.EvalSeg(spos, null, j, tr); 
-                    
-                    float currstrokew = SketchLineStyle.strokew; 
-                    Shape spike = new Rectangle2D.Float((float)spos.getX() - 2 * currstrokew, (float)spos.getY() - 2 * currstrokew, 4 * currstrokew, 4 * currstrokew);
-                    ga.drawShape(spike, null, Color.red);
-                }
-            }
+            Point2D spos = tnc.spikelist.get(i).spos; 
+            Shape spike = new Rectangle2D.Float((float)spos.getX() - 2 * currstrokew, (float)spos.getY() - 2 * currstrokew, 4 * currstrokew, 4 * currstrokew);
+            ga.drawShape(spike, null, (tnc.spikelist.get(i).batend ? Color.blue : Color.red));
         }
     }
 }
