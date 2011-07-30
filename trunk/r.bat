f49cfe2452449320394e1a24baa6895d926a5826
goatchurch <goatchurch@goatchurch-PC>
2011-07-30 08:45:56 +0200

    sketch frames with elevations in same coord space

--- trunk/r.bat
+++ trunk/r.bat
@@ -1,2 +1,2 @@
 REM java -showversion -ea -Xmx1000M -cp . Tunnel.MainBox C:\\Users\\goatchurch\\tunneldata
-java -showversion -ea -Xmx1000M -cp . Tunnel.MainBox C:\\Users\\goatchurch\\tunneldata\\204-piece-frame.xml
+java -showversion -ea -Xmx1000M -cp . Tunnel.MainBox C:\\Users\\goatchurch\\tunneldata\\204-piece-frame-side.xml
