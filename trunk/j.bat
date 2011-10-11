84fe3eac771894621d824e85a7e1774482483c0a
Julian Todd <julian@goatchurch.org.uk>
2011-10-11 10:44:54 +0100

    fix jar building so it includes help.html

--- trunk/j.bat
+++ trunk/j.bat
@@ -1,5 +1,6 @@
 dir/b symbols > symbols\listdir.txt
-"C:\Program Files\Java\jdk1.6.0_26\bin\jar" cmf tunnelmanifest.txt tunnel.jar Tunnel symbols/*.xml symbols/listdir.txt
-"C:\Program Files\Java\jdk1.6.0_26\bin\jar" i tunnel.jar
+dir/b tutorials > tutorials\listdir.txt
+"C:\Program Files\Java\jdk1.6.0_26\bin\jar" cmf tunnelmanifest.txt tunnel.jar Tunnel symbols/*.xml symbols/*.html symbols/listdir.txt tutorials/*.xml tutorials/listdir.txt
+REM "C:\Program Files\Java\jdk1.6.0_26\bin\jar" i tunnel.jar
 REM "C:\Program Files\Java\jdk1.6.0\bin\jar" tf tunnel.jar
 
