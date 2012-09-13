bed8130640b866fc23b00790126d8369cd19a96d
Andrew Atkinson <AndrewAkinson@users.noreply.github.com>
2012-09-13 07:13:35 +0100

    Added code to decode proprietory .top files
    user: Andrew Atkinson
    changed trunk/src/TunnelTopParser.java

--- trunk/src/TunnelTopParser.java
+++ trunk/src/TunnelTopParser.java
@@ -24,6 +24,7 @@ import java.awt.geom.Line2D;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Date;
 
 import java.awt.Font;
 import java.awt.Color;
@@ -38,31 +39,109 @@ import java.io.FileReader;
 import java.io.StreamTokenizer;
 import java.io.InputStreamReader;
 
+class TOPxsection
+{
+	int x;
+	int y;
+	String stn;
+	float direction;
 
+	TOPxsection(InputStream inp) throws IOException
+	{
+		x = TunnelTopParser.ReadInt4(inp);
+		y = TunnelTopParser.ReadInt4(inp);
+		stn = TunnelTopParser.ReadStn(inp);
+		int idirection = TunnelTopParser.ReadInt4(inp);
+		if (idirection != -1)
+			direction = TunnelTopParser.adegrees(idirection);
+		else
+			direction = -1.0f;
+		System.out.println("Xsection "+ x +" "+y+" "+stn+" "+direction);
+	}
+}
+
+class TOPpolygon
+{
+	int[] poly;
+	int col;
+
+	TOPpolygon(InputStream inp) throws IOException
+	{
+		int a = TunnelTopParser.ReadInt4(inp);
+		poly = new int[a*2];
+		for (int i = 0; i <a; i++)
+		{
+			poly[i * 2] = TunnelTopParser.ReadInt4(inp);
+			poly[i * 2 +1] = TunnelTopParser.ReadInt4(inp);
+			System.out.println("Polygon\n"+ poly[i * 2] +" "+ poly[i * 2+1]);
+		}
+		col = inp.read();
+		System.out.println(col);	 
+	}
+}
 /////////////////////////////////////////////
 class TunnelTopParser extends TunnelXMLparsebase
 {
 	OneSketch tunnelsketch = null;
-	int version; 
+	int version;
+	List<TOPxsection> planxsections = new ArrayList<TOPxsection>();
+	List<TOPxsection> elevxsections = new ArrayList<TOPxsection>();
+	List<TOPpolygon> planpolygons = new ArrayList<TOPpolygon>();
+	List<TOPpolygon> elevpolygons = new ArrayList<TOPpolygon>();
+	
+
+	/////////////////////////////////////////////	
+	static float adegrees(int bangle)
+	{
+		return 360*(float)bangle / 65536;
+	}
 
 	/////////////////////////////////////////////
-	int ReadInt(InputStream inp) throws IOException
+	static int ReadInt2(InputStream inp) throws IOException
+	{
+		int b0 = inp.read();
+		int b1 = inp.read();
+
+		int res = b0 + (b1 << 8); 
+//System.out.println("eee "+b0+" "+b1+" "+res); 
+		return res; 
+	}
+	/////////////////////////////////////////////
+	static int ReadInt4(InputStream inp) throws IOException
 	{
 		int b0 = inp.read();
 		int b1 = inp.read();
 		int b2 = inp.read();
 		int b3 = inp.read();
 		int res = b0 + (b1 << 8) + (b2 << 16) + (b3 << 24); 
-System.out.println("eee "+b0+" "+b1+" "+b2+" "+b3+"   "+res); 
+//System.out.println("eee "+b0+" "+b1+" "+b2+" "+b3+"   "+res); 
+		return res; 
+	}
+
+	long ReadInt8(InputStream inp) throws IOException
+	{
+		long b0 = inp.read();
+		long b1 = inp.read();
+		long b2 = inp.read();
+		long b3 = inp.read();
+		long b4 = inp.read();
+		long b5 = inp.read();
+		long b6 = inp.read();
+		long b7 = inp.read();
+		long res = b0 + (b1 << 8) + (b2 << 16) + (b3 << 24) + (b4 << 32) + (b5 << 40) + (b6 << 48) + (b7 << 56); 
+//System.out.println("eee "+b0+" "+b1+" "+b2+" "+b3+"   "+res); 
 		return res; 
 	}
 
 	/////////////////////////////////////////////
 	int ReadDate(InputStream inp) throws IOException
-	{
-		byte[] sdate = new byte[8]; 
-		inp.read(sdate, 0, 8);  
-		System.out.println(ReadInt(inp)+" "+ReadInt(inp)); 
+	{  
+		long i1 = ReadInt4(inp);
+		long i2 = ReadInt4(inp);
+		long si = i1 + (i2<<32);
+		long sit = (si - 621355968000000000L) / 10000;
+		Date date = new Date(sit);
+		System.out.println(date);  
 		return 0; 
 //		ticks =  struct.unpack('<Q', F.read(8))
 //		#Need to convert this date from .NET
@@ -75,18 +154,76 @@ System.out.println("eee "+b0+" "+b1+" "+b2+" "+b3+"   "+res);
 	/////////////////////////////////////////////
 	String ReadComments(InputStream inp) throws IOException
 	{
-	    int commentlength = inp.read(); 
-	    if (commentlength >= 128)
-        {
-			int commentlength2 = inp.read(); 
-			commentlength = commentlength - 128 + 128*commentlength2; 
-	        assert commentlength2 < 128; 
+		int commentlength = 0;
+		int cbyte =1;
+		while (true)
+		{
+			int commentbyte = inp.read();
+			commentlength += (commentbyte & 127) * cbyte;
+			if (commentbyte < 128)
+				break;
+			cbyte *= 128;
 		}
-System.out.println("Commentlength "+commentlength);  
-		byte[] cstr = new byte[commentlength];
-		return new String(cstr); 
+	    
+System.out.println("Commentlength "+commentlength);
+		if (commentlength == 0)
+				return "";
+		else
+		{  
+			byte[] cstr = new byte[(int)commentlength];
+			inp.read(cstr, 0, commentlength);
+			return new String(cstr,"UTF8");
+		}
 	}
 
+	/////////////////////////////////////////////
+	/////////////////////////////////////////////
+	/////////////////////////////////////////////
+	/////////////////////////////////////////////
+
+	/////////////////////////////////////////////
+	static String ReadStn(InputStream inp) throws IOException
+	{
+		//id's split into major.decimal(minor)
+		int idd = ReadInt2(inp);
+		int idm = ReadInt2(inp);
+		//Turn stn into string, andnulls into -
+		if (idm == 32768)
+			return new String("-");
+		else
+			return String.valueOf(idm)+"."+String.valueOf(idd);
+
+
+	}
+	/////////////////////////////////////////////
+	/////////////////////////////////////////////
+	/////////////////////////////////////////////
+	void drawing(List<TOPxsection> xsections, List<TOPpolygon> polygons, InputStream inp) throws IOException
+	{
+		mapping(inp);
+		while (true)
+		{
+			int element = inp.read();
+			if (element == 0)
+				break;
+			if (element == 1)
+				polygons.add(new TOPpolygon(inp));
+			else if (element == 3)
+				xsections.add(new TOPxsection(inp));
+			else
+				TN.emitError("Element number not defined");
+		}
+			
+	}
+	/////////////////////////////////////////////
+	void mapping(InputStream inp) throws IOException
+	{
+		//Gets the centre point of screen and scale of different views
+		int X = ReadInt4(inp);
+		int Y = ReadInt4(inp);
+		int scale = ReadInt4(inp);
+		System.out.println(X +" "+ Y +" "+ scale);
+	}			
 	/////////////////////////////////////////////
 	boolean ParseFile(FileAbstraction tfile)
 	{ try {
@@ -98,18 +235,61 @@ System.out.println("Commentlength "+commentlength);
 
         version = inp.read(); 
         TN.emitWarning("We have a top file version " + version); 
-		int ntrips = ReadInt(inp); 
+		int ntrips = ReadInt4(inp); 
 		for (int i = 0; i < ntrips; i++)
 		{
 			ReadDate(inp); 
 			String comments = ReadComments(inp); 
 			System.out.println("::"+comments+"::"); 
-			int declination = (inp.read()<<8) + inp.read(); 
+			float declination = adegrees(ReadInt2(inp)); 
 			System.out.println("declination "+declination); 
 		}
-		for (int i = 0; i < 10; i++)
-			System.out.println(i+"  "+inp.read()); 
-        inp.close(); 
+		//legs/shots
+		int nshots = ReadInt4(inp);
+		System.out.println("FromStn ToStn Dist comp clino ");
+		for (int i = 0; i < nshots; i++)
+		{
+			//Station
+			String fromstn = ReadStn(inp);
+			String tostn = ReadStn(inp);
+			int dist = ReadInt4(inp);
+			float azimuth = adegrees(ReadInt2(inp));
+			float inclination = adegrees(ReadInt2(inp));
+			int flags = inp.read();
+			int roll = inp.read();
+			int tripindex = ReadInt2(inp);
+		    //bit 1 of flags is flip (left or right)
+    		//bit 2 of flags indicates a comment
+			String comment = "";
+    		if ((flags & 2)  == 2)
+			{
+				comment = ReadComments(inp);
+			}
+			
+			System.out.println(fromstn +" "+ tostn +" "+ dist +" "+ azimuth +" "+inclination +" "+ flags +" "+ roll +" "+ tripindex+" "+ comment);
+		}
+		//Reference sations
+		int nrefstn = ReadInt4(inp);
+		System.out.println("Stn NS EW");
+		for (int i = 0; i < nrefstn; i++)				
+		{
+			String stn = ReadStn(inp);
+			long east = ReadInt8(inp);
+			long west = ReadInt8(inp);
+			int altitute =ReadInt2(inp);
+			String comment = ReadComments(inp);
+
+		}
+
+		//Overview Mapping information (not needed by import)
+		mapping(inp);
+
+		//Plan (outline)
+		drawing(planxsections, planpolygons, inp);
+		//Elevation (sideview)
+		drawing(elevxsections, elevpolygons, inp);
+
+        inp.close();
 	}
 	catch (IOException e)
 	{
