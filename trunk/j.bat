e47f77c00c440d84343b6b667c8ebdeb23b8dc04
Julian Todd <julian@goatchurch.org.uk>
2012-12-24 19:43:27 +0100

    remove listdir.txt

--- trunk/j.bat
+++ trunk/j.bat
@@ -1,4 +1,4 @@
-REM dir/b symbols > symbols\listdir.txt
+dir/b symbols > symbols\listdir.txt
 dir/b tutorials > tutorials\listdir.txt
 "C:\Program Files\Java\jdk1.6.0_26\bin\jar" cmf tunnelmanifest.txt tunnel.jar Tunnel symbols/*.xml symbols/*.html symbols/listdir.txt tutorials/*.* tutorials/listdir.txt
 REM "C:\Program Files\Java\jdk1.6.0_26\bin\jar" i tunnel.jar
