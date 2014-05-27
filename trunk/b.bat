c75924b160faca2731fd7c9eee270edd613b746f
Julian Todd <julian@goatchurch.org.uk>
2014-05-27 08:58:03 +0200

    added output into XML form for BGS in OSGB coords of centreline

--- trunk/b.bat
+++ trunk/b.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Java\jdk1.6.0_26\bin\javac" -target 1.5 -Xlint:deprecation -d . src\*.java
+"C:\Program Files\Java\jdk1.7.0_17\bin\javac" -source 1.5 -target 1.5 -Xlint:deprecation -d . src\*.java
